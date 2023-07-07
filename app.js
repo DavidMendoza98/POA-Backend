fc7bdffd66f11e9b529ff7fc92696452190657a8
David Mendoza <david.mendoza@unah.hn>
2023-07-07 16:07:47 -0600

    Cambiando puerto

--- app.js
+++ app.js
@@ -89,7 +89,7 @@ const db = require("./models/");
 //require("./routes/users")(app);
 
 // set port, listen for requests
-const PORT = 8080;
+const PORT = 3000;
 //process.env.PORT || 8080
 app.listen(PORT ,() => {
   console.log(`Server is running on port ${PORT}.`);
